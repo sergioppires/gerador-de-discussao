6ac8a026924094086124cde1df5b760ffb1ddb3a
Sergio Pires <37277843+sergioppires@users.noreply.github.com>
2018-08-10 20:22:17 -0300

    Update Connections.java

--- ProjetoFinalGUI/src/main/Connections.java
+++ ProjetoFinalGUI/src/main/Connections.java
@@ -10,10 +10,10 @@ public class Connections {
     	// Fazer a conexao
     	ConfigurationBuilder cb = new ConfigurationBuilder();
 	    cb.setDebugEnabled(true)
-	      .setOAuthConsumerKey("gLouypIJg3NtI6EDgtUQROKPY")
-	      .setOAuthConsumerSecret("lEyUxLnSQsi8xWdAjVJvSHa1xbzFE9WxhYhLxNsjjh3NFdJORw")
-	      .setOAuthAccessToken("56293343-2Wmz9J22bQ8oyUIVgKOr5QXikOPS94y72jwUvLvbW")
-	      .setOAuthAccessTokenSecret("gW6qmtD2y4KIobCU7aSzptZTiGO0Rg17Mexxsq930etx3");
+	      .setOAuthConsumerKey("xxxxxx") 
+	      .setOAuthConsumerSecret("xxxxx")
+	      .setOAuthAccessToken("xxxxxx")
+	      .setOAuthAccessTokenSecret("xxxxxxx");
 	    Twitter twitter = new TwitterFactory(cb.build()).getInstance();  
 	    return twitter;
         }
